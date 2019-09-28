72a1ff3c4498fb20b2575b5b81e4d743f6797272
16hchung <16hchung@gmail.com>
2019-09-27 20:57:19 -0700

    made changes to training and model

--- blackjack/model/rl_model/decider.py
+++ blackjack/model/rl_model/decider.py
@@ -22,13 +22,16 @@ n_actions = 2 # don't use player.Action enum bc don't want circular dependency
 class DQN(nn.Module):
 	def __init__(self):
 		super().__init__()
-		self.fc1 = nn.Linear(input_dim, 10)
-		self.fc2 = nn.Linear(10, n_actions)
+		self.fc1 = nn.Linear(input_dim, 20)
+		self.fc2 = nn.Linear(20, 10)
+		self.fc3 = nn.Linear(10, n_actions)
 		self.dropout = nn.Dropout(p=.1)
 
 	def forward(self, x):
 		x = self.fc1(x)
 		x = self.dropout(x)
-		x = torch.tanh(x)
+		x = F.elu(x)
 		x = self.fc2(x)
-		return torch.tanh(x)
+		x = F.elu(x)
+		x = self.fc3(x)
+		return x
--- blackjack/model/rl_model/train.py
+++ blackjack/model/rl_model/train.py
@@ -11,12 +11,13 @@ import torch.nn.functional as F
 
 from .decider import DQN
 
-BATCH_SIZE = 128
+BURN_PERIOD = 50000
+BATCH_SIZE = 128*4
 GAMMA = 0.999
 EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 200
-TARGET_UPDATE = 10
+TARGET_UPDATE = 5
 
 Transition = namedtuple('Transition',
 	('state', 'action', 'next_state', 'reward')
@@ -44,36 +45,37 @@ class ReplayMemory:
 class TrainLogger:
 	def __init__(self, info_keys, log_freq=100):
 		self.all_info = pd.DataFrame(columns=info_keys  + ['iter'])
-		self.avgd_info = pd.DataFrame(columns=info_keys + ['iter'])
 		self.iter = 0
 		self.log_freq = log_freq
 
 	def log(self, info):
-		if self.iter % self.log_freq == 0:
+		try:
 			info['iter'] = self.iter
 			self.all_info = self.all_info.append(info, ignore_index=True)
-			print(info)
-		self.iter += 1
-		# add new row to all_info
-		# only take last <= 100 (index from max(len-100, 0) to current index)
-		# take average
-		# add to avgd_info
-		pass
+			self.iter += 1
+			if self.iter % self.log_freq == 0:
+				print(info)
+		except:
+			pass
 
 	def plot(self, col_names):
 		pass
 
 	def finish(self, fname):
-		self.all_info.to_csv('all_'+fname)
-		#self.avgd_info.to_csv('avg_')
-		pass
+		try:
+			self.all_info.to_csv('indv_'+fname)
+			moving_avged = self.all_info.rolling(window=100, min_periods=1).mean()
+			moving_avged.to_csv('avged_'+fname)
+		except:
+			pass
 
 def select_action(state, device, policy_net, env, steps_done):
 	n_actions = env.action_space.n
 	sample = random.random()
+	steps_done = max(0, steps_done - BURN_PERIOD)
 	eps_threshold = EPS_END + (EPS_START - EPS_END) * \
 		math.exp(-1. * steps_done / EPS_DECAY)
-	if sample > eps_threshold:
+	if sample > eps_threshold and steps_done > 0:
 		with torch.no_grad():
 			return policy_net(state).max(0)[1].view(1, 1), False
 	else:
@@ -118,7 +120,7 @@ def optimize_model(policy_net, target_net, optimizer, memory, device):
 	optimizer.step()
 	return loss.item()
 
-def train(num_episodes):
+def train(num_episodes, out_params_file, log_file):
 	env = gym.make('Blackjack-v1')
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -129,58 +131,75 @@ def train(num_episodes):
 	target_net.load_state_dict(policy_net.state_dict())
 	target_net.eval()
 
-	optimizer = optim.RMSprop(policy_net.parameters())
-	memory = ReplayMemory(10000)
+	#optimizer = optim.RMSprop(policy_net.parameters(), lr=.00001, weight_decay=.001)
+	optimizer = optim.Adam(policy_net.parameters(), lr=.001, weight_decay=.01)
+	memory = ReplayMemory(20000)
 
 	info_keys = env.info_keys + ['reward', 'action']
 	logger = TrainLogger(info_keys)
 
 	steps_done = 0
+	loss = None
 	for i_episode in range(num_episodes):
 		# Init environment and state
 		state = torch.from_numpy(env.reset()).float().to(device)
 		for t in count():
 			# Select and perform action
-			action, was_rand = select_action(state, device, policy_net, env, steps_done)
-			next_state, reward, done, info = env.step(action.item())
-			# Log info for plotting
-			more_info = {
-				'action': action.item(),
-				'reward': reward,
-				'rand_action': was_rand
-			}
-			# Turn output to tensors
-			reward = torch.tensor([reward], device=device).float()
-			next_state = torch.from_numpy(next_state).float().to(device)
-			if done: next_state = None
-			steps_done += 1
+			step_outpts = []
+			reward = 0; done = False
+			orig_state = state
+			while reward == 0 and not done:
+				action, was_rand = select_action(state, device, policy_net, env, steps_done)
+				step_outpt = env.step(action.item())
+				[next_state, reward, done, _] = step_outpt
+				step_outpts.append(step_outpt)
+				state = torch.from_numpy(next_state).float().to(device)
+				steps_done += 1
 
-			# Store transition in memory
-			memory.push(state, action, next_state, reward)
+			reward = step_outpts[-1][1] # if hit and don't bust, use ultimate outcome as reward
+			reward_t = torch.tensor([reward], device=device).float()
+			state = orig_state
+			for [next_state, _, done, info] in step_outpts:
+				# Log info for plotting
+				more_info = {
+					'action': action.item(),
+					'reward': reward,
+					'rand_action': was_rand
+				}
+				# Turn output to tensors
+				next_state = torch.from_numpy(next_state).float().to(device)
+				if done: next_state = None
 
-			# Move to next state
-			state = next_state
+				# Store transition in memory
+				memory.push(state, action, next_state, reward_t)
 
-			loss = optimize_model(policy_net, target_net, optimizer, memory, device)
-			more_info['loss'] = loss
-			info.update(more_info)
-			logger.log(info)
+				# Move to next state
+				state = next_state
+
+				more_info['loss'] = loss
+				info.update(more_info)
+				logger.log(info)
 			if done:
+				loss = optimize_model(policy_net, target_net, optimizer, memory, device)
 				break
 		# Update target network
 		if i_episode % TARGET_UPDATE == 0:
 			target_net.load_state_dict(policy_net.state_dict())
-	logger.finish('train_log.csv')
+
+	torch.save(policy_net.state_dict(), out_params_file)
+	logger.finish(log_file)
 
 if __name__=='__main__':
 	import argparse
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--n_eps', type=int, default=10000000)
+	parser.add_argument('--n_eps', type=int, default=10000)
+	parser.add_argument('--out_params', default='params_state_dict.pkl')
+	parser.add_argument('--log_file', default='train_log.csv')
 	args = parser.parse_args()
 
 	import time
 	start = time.time()
-	train(args.n_eps)
+	train(args.n_eps, args.out_params, args.log_file)
 	print('finished in {} minutes'.format((time.time() - start) / 60))
 	# TODO: save parameters
 
